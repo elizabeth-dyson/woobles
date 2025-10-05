f6b579d3d02f66b79a5e491f12b0ebc018f0a210
Elizabeth.Dyson <Elizabeth.Dyson@kiewit.com>
2025-10-05 13:08:20 -0500

    box 4 timeline added

--- xmas_2025/fourth_box.py
+++ xmas_2025/fourth_box.py
@@ -37,7 +37,38 @@ with text_c:
 
     st.subheader("Timeline")
 
+    st.write("HEAD & BODY")
+    st.write("- 20 rounds")
+    st.write("- 10/5: Rounds 1-4")
+    st.write("- 10/6: Rounds 5-8")
+    st.write("- 10/7: Rounds 9-12")
+    st.write("- 10/8: Rounds 13-16")
+    st.write("- 10/9: Rounds 17-20")
 
+    st.write("SNOUT")
+    st.write("- 3 rounds")
+    st.write("- 10/10: Rounds 1-3")
+
+    st.write("EARS")
+    st.write("- 2 rounds x2")
+    st.write("- 10/11: First Ear")
+    st.write("- 10/12: Second Ear")
+
+    st.write("LEGS")
+    st.write("- 2 rounds (x4)")
+    st.write("- 10/13: First Leg")
+    st.write("- 10/14: Second Leg")
+    st.write("- 10/15: Third Leg")
+    st.write("- 10/16: Fourth Leg")
+
+    st.write("NOSE & MOUTH")
+    st.write("- 5 steps")
+    st.write("- 10/17: Steps 1-5")
+
+    st.write("HAIR")
+    st.write("- 2 rows")
+    st.write("- 10/18: Row 1")
+    st.write("- 10/19: Row 2")
 
     st.subheader("Completed on ")
 
